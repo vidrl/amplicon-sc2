b548dc169945116b3166b567c8f1b38bd0e408bd
Josh Zhang <zjs0317@gmail.com>
2026-07-07 14:40:20 +1000

    bug fix 20260707 (#19)
    * Make plot spacing and dimensions dynamic
    
    * Updated vertical and horizontal spacing to be dynamic based on the number of rows and columns. Adjusted layout dimensions accordingly.
    
    * Modify coverage plots section layout in HTML template
    
    * Updated styling for coverage plots section in report template to prevent plots cutted off
    
    * Downsample pool data and update hover settings
    
    * Downsampled data for pools 1 and 2 by taking every 10th entry to improve performance. Updated hover information settings to compress the size of the html report
    
    * Typo fix on variable name
    
    
    ---------
    
    Co-authored-by: Ammar Aziz <spidermine63@gmail.com>

--- modules/local/generate_run_report/templates/generate_run_report.py
+++ modules/local/generate_run_report/templates/generate_run_report.py
@@ -186,7 +186,7 @@ def wf_coverage_plots(
     #josh 20260706
 
     if nrows > 1:
-        max_spacing = 1 / (num_rows -1)
+        max_spacing = 1 / (nrows -1)
         dynamic_v_spacing = max_spacing * 0.8
     else:
         dynamic_v_spacing = 0.0
@@ -197,7 +197,7 @@ def wf_coverage_plots(
         dynamic_h_spacing = 0.0
 
     dynamic_height = max(600, nrows * 300)
-    dynamic_weight = max(500, ncols * 240)
+    dynamic_width = max(500, ncols * 240)
 
     fig = make_subplots(
         rows=nrows,
@@ -1303,7 +1303,7 @@ if coverage_data.exists():
         coverage_data,
         threshold=20,  # min depth - params.min_coverage_depth
         xlim=30000,  # size of genome
-        ylim=800,  # normalise depth x 2 - params.normalise_depth #may need to change to a smaller number
+        ylim=400,  # normalise depth x 2 - params.normalise_depth #may need to change to a smaller number
         ncols=3,  # how many columns per page
     )
 
